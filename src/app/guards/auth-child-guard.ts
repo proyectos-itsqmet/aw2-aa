225b0d8d1545915e3dfac5141e71d365ca268b30
Gerardo Araque <garaque@profermaco.com.ec>
2026-02-19 12:25:35 -0500

    Logs
    Logs

--- src/app/guards/auth-child-guard.ts
+++ src/app/guards/auth-child-guard.ts
@@ -6,6 +6,10 @@ export const authGuardChildGuard: CanActivateFn = (_route, _state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
+  console.log(`[Guard] canActivateChild → ${_state.url}`);
+  console.log(`[Guard] Ruta hija: ${_route.routeConfig?.path}`);
+  console.log(`[Guard] Data: `, _route.data);
+
   if (authService.isAuthenticated()) {
     return true;
   }
